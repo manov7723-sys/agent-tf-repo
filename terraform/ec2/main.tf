45bbf91f04e690f8198c93e3def1c25bf9f3e8e2
manov7723-sys <manov7723@gmail.com>
2026-06-05 16:22:03 +0530

    Create EC2 instance with Ubuntu 22.04 LTS

---
+++ terraform/ec2/main.tf
@@ -0,0 +1,11 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-0c94855ba95c71c99"
+  instance_type = "t3.micro"
+  root_block_device {
+    volume_size = 8
+  }
+}
