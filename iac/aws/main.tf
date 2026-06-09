c2e1ebff3aae76f2fe1f8e1bed62c53f87874364
manov7723-sys <manov7723@gmail.com>
2026-06-09 16:58:03 +0530

    Update main.tf

--- iac/aws/main.tf
+++ iac/aws/main.tf
@@ -1,11 +1,1 @@
-provider "aws" {
-  region = "us-east-1"
-}
 
-resource "aws_instance" "example" {
-  ami           = "ami-0c94855ba95c71c99"
-  instance_type = "t3.medium"
-  root_block_device {
-    volume_size = 50
-  }
-}
